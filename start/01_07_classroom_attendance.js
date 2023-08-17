e603521d12c5f1a8454816ccd4af3985c5a7fd97
Dat T. Tran <datttrian@gmail.com>
2023-08-17 16:52:05 -0400

    destructure values

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,19 @@
 // Write your code here
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teacherAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teacherAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(
+  getStudents({
+    hasTeachingAssistant: false,
+    classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"],
+  })
+);
